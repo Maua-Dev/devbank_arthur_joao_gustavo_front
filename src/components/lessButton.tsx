8132f40c6a32514d194153bb958b9215b62a14d3
Gustavo <gustavoseripierri@gmail.com>
2024-04-18 23:02:00 -0300

    la ele

---
+++ src/components/lessButton.tsx
@@ -0,0 +1,17 @@
+import '../components/lessButton.css'
+
+type lessButtonProps = {
+    numero: string;
+}
+
+const lessButton = ({numero}: lessButtonProps) => {
+    const handleClick = () =>{
+        numero += numero
+        console.log(numero)
+    }
+    return(
+        <button onClick={handleClick}> {numero} </button>
+    )
+}
+
+export default lessButton
--- src/pages/DepositPage.tsx
+++ src/pages/DepositPage.tsx
@@ -1,9 +1,62 @@
 import '../styles/Depositpage.css'
+import '../components/lessButton.css'
+import '../components/sumButton.css'
 
 function DepositPage() {
   return (
-    <div>
+    <div className='deposito'>
       <h1>Pagina de deposito</h1>
+      <div className='block'>
+       <h1>R$ 2</h1>
+       <section>
+        <button className="buttondeposit">-</button>
+        <button className="buttondeposit">+</button>
+       </section>
+       
+      </div>
+      <div className='block'>
+       <h1>R$ 5</h1>
+       <section>
+        <button className="buttondeposit">-</button>
+        <button className="buttondeposit">+</button>
+       </section>
+      </div>
+      <div className='block'>
+       <h1>R$ 10</h1>
+       <section>
+        <button className="buttondeposit">-</button>
+        <button className="buttondeposit">+</button>
+       </section>
+      </div>
+      <div className='block'>
+       <h1>R$ 20</h1>
+       <section>
+        <button className="buttondeposit">-</button>
+        <button className="buttondeposit">+</button>
+       </section>
+      </div>
+      <div className='block'>
+       <h1>R$ 50</h1>
+       <section>
+        <button className="buttondeposit">-</button>
+        <button className="buttondeposit">+</button>
+       </section>
+      </div>
+      <div className='block'>
+       <h1>R$ 100</h1>
+       <section>
+        <button className="buttondeposit">-</button>
+        <button className="buttondeposit">+</button>
+       </section>
+      </div>
+      <div className='block'>
+       <h1>R$ 200</h1>
+       <section>
+        <button className="buttondeposit">-</button>
+        <button className="buttondeposit">+</button>
+       </section>
+      </div>
+      
     </div>
   );
 }
